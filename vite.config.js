42af2eb7d976dfb5d2703e148e07f2e624fd34f4
‘xforg’ <oppoa1001@hotmail.com>
2026-02-06 07:28:18 +0800

    Add base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/kids-english-app/',
   server: {
     port: 3000,
     open: true // 启动时自动打开浏览器
